0a745a23d3ad8b53d055fe0641142241650d31f6
RMGeorgetti <rodrigogeorgetti.cba@gmail.com>
2023-06-09 00:17:28 -0300

    01-Node

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -1,11 +1,27 @@
-const process = require('process');
-const { Z_ASCII } = require('zlib');
-const commands = require('./commands/index.js');
+const process = require("process");
+const { Z_ASCII } = require("zlib");
+const commands = require("./commands/index.js");
 
-function bash() {}
+function print(output) {
+  process.stdout.write(output);
+  process.stdout.write("\nprompt > ");
+}
+
+function bash() {
+  process.stdout.write("prompt > ");
+  process.stdin.on("data", (data) => {
+    const args = data.toString().trim().split(" ");
+    const cmd = args.shift();
+    if (commands.hasOwnProperty(cmd)) {
+      commands[cmd](print, args.join(" "));
+    } else {
+      print(`command not found: ${cmd}`);
+    }
+  });
+}
 
 bash();
 module.exports = {
-   print,
-   bash,
+  print,
+  bash,
 };
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -2,20 +2,51 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+  print(process.cwd());
+}
 
-function date() {}
+function date(print) {
+  print(Date());
+}
 
-function echo() {}
+function echo(print, args) {
+  print(args);
+}
 
-function ls() {}
+function ls(print) {
+  fs.readdir(".", (error, files) => {
+    if (error) throw Error(error);
+    print(files.join(" "));
+  });
+}
 
-function cat() {}
+function cat(print, args) {
+  fs.readFile(args, "utf-8", (error, data) => {
+    if (error) throw Error(error);
+    print(data);
+  });
+}
 
-function head() {}
+function head(print, args) {
+  fs.readFile(args, "utf-8", (error, data) => {
+    if (error) throw Error(error);
+    print(data.split("\n").at(0).trim());
+  });
+}
 
-function tail() {}
+function tail(print, args) {
+  fs.readFile(args, "utf-8", (error, data) => {
+    if (error) throw Error(error);
+    print(data.split("\n").at(-1).trim());
+  });
+}
 
-function curl() {}
+function curl(print, args) {
+  utils.request(`https://${args}`, (error, response) => {
+    if (error) throw Error(error);
+    print(response);
+  });
+}
 
-module.exports = {};
+module.exports = { pwd, date, echo, ls, cat, head, tail, curl };
