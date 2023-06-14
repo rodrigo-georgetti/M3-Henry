6cb9cbd915046cac8804072988345cd9e95fc1cb
RMGeorgetti <rodrigogeorgetti.cba@gmail.com>
2023-06-14 14:50:31 -0300

    Webserver

--- 02-WebServer/homework/01 - Exercises/server.js
+++ 02-WebServer/homework/01 - Exercises/server.js
@@ -3,11 +3,13 @@ var http = require("http");
 /* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
 const PORT = 3001;
+
 const server = http
   .createServer((req, res) => {
     console.log(`Server raised in port ${PORT}`);
-    switch (req.url){
-      case '/api':
+
+    switch (req.url) {
+      case "/api":
         fs.readFile("./utils/dogsData.json", (err, data) => {
           if (err) {
             res.writeHead(404, { "Content-Type": "text/plain" });
@@ -18,25 +20,24 @@ const server = http
           }
         });
         return;
-        case '/allDogs':
-          fs.readFile('./utils/allDogs.html', 'utf8', (err, data) => {
-            if (err) {
-              res.writeHead(404, { "Content-Type": "text/plain" });
-              res.end("html not found");
-            } else {
-              res.writeHead(200, { "Content-Type": "text/html" });
-              res.end(data);
-            }
-          })
-          return
-          default :
-          res.writeHead(404, { "Content-Type": "text/plain" });
-              res.end("Route not found");
+      case "/allDogs":
+        fs.readFile("./utils/allDogs.html", "utf8", (err, data) => {
+          if (err) {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("html not found");
+          } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+          }
+        });
+        return;
+      default:
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Route not found");
     }
   })
-  .listen(PORT, "localhost"); //aca da el error de test
+  .listen(PORT, "localhost");
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
-module.exports = {
+module.exports =
   /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
-  server
-}; // esto era null
+  server;
